0dec1643076328e471607159e7849ef02d04ad0f
peaceful-state-9936 <efua.mensahbrown@gmail.com>
2019-04-09 13:53:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ if (name==="Susan"){
 }else{
   console.log("Expected the name to be Susan")
 }
-if (height==="74"){
+if (height==="70"){
   console.log("The height is correct")
 }else{
-  console.log("Expected height is 74")
+  console.log("Expected height is 70")
 }
 
 // Don't worry about this
